9ad745f3b8b1995e979b8f84c42d0b011696c906
John Sabbá <victorsabba@gmail.com>
2019-07-01 15:06:58 -0300

    Bora trabalhar senao essa porra não sai

---
+++ Application/Model/User.php
@@ -0,0 +1,16 @@
+<?php
+use SGE\Database\Connection;
+
+class User{
+    
+    public function login($data){
+       $DB = new Connection;
+       $sql = "";
+       $rows = $DB->query($sql);
+        # Valida login 
+
+        # retorna o id do usuário 
+
+
+    }
+}
