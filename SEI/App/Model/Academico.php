9b1b63e2e85ffb19ba69cb2033caecc487365f2c
Leonardo de Castro <leonardodecastrosc@gmil.com>
2019-08-15 08:03:29 -0300

    Add Hour view to dashboard

--- SEI/App/Model/Academico.php
+++ SEI/App/Model/Academico.php
@@ -1,7 +1,20 @@
 <?php
+
 use Livro\Database\Record;
+use Livro\Database\Criteria;
+use Livro\Database\Repository;
+use Livro\Database\Transaction;
 
 class Academico extends Record
 {
     const TABLENAME = 'academico';
+
+    public static function find2($cpf){
+        Transaction::open('sei');
+        $aux = Transaction::get();
+        $sql = "select * from academico where pessoa_cpf='$cpf'";
+        $result = $aux->query($sql);
+        return $result->fetchObject(__CLASS__);
+    }
+
 }
