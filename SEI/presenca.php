2c819c78c3ce1d1d10b291fdfc3d5b2c430db42b
Leonardo de Castro <leonardodecastrosc@gmil.com>
2019-08-26 15:17:11 -0300

    lets go

--- SEI/presenca.php
+++ SEI/presenca.php
@@ -7,8 +7,16 @@ function givePresenca($id,$cpf){
     $sql = "SELECT * FROM pessoa_has_evento WHERE pessoa_cpf='$cpf' AND evento_id=$id";
     $result = $db->query($sql);
     $gets = $result->num_rows;
+    $g = $result->fetch_assoc();
+    if($g['presenca']=='s'){
+        setcookie('evento',null, time() - 3600);
+        unset($_COOKIE['evento']);
+        session_destroy();
+        return FALSE;
+    }
     $nowh = date('H:i:s');
     $nowd = date('Y-m-d');
+    
     if($gets == 0){
         $sql = "INSERT INTO pessoa_has_evento(pessoa_cpf,evento_id,presenca,ordem,datainscricao,horainscricao)VALUES('$cpf',$id,'s',1,'$nowd','$nowh')";
         if($db->query($sql)=== TRUE){
@@ -36,9 +44,14 @@ if(!isset($_COOKIE['cpf']) || empty($_COOKIE['cpf'])){
     header('location:index.php');
 }
 
+if(isset($_GET['code']) && !isset($_COOKIE['evento'])){
+    $code =  $_GET['code'];
+    setcookie('evento',$code);
+}
+if(isset($_COOKIE['evento'])){
+    $code = $_COOKIE['evento'];
+}
 
-$code =  $_GET['code'];
-setcookie('evento',$code);
 $cpf = $_COOKIE['cpf'];
 $sql = "SELECT id,inicio,tipo_id FROM evento WHERE code='$code'";
 $result = $db->query($sql);
@@ -60,35 +73,37 @@ $nowh = date('H:i:s');
 $twoplus = date('H:i:s',strtotime($inicio)+7200);
 if($tipo == 11){
     if(givePresenca($id,$cpf)){
-        setcookie('evento',null, -1, '/');
+        setcookie('evento',null, time() - 3600);
         unset($_COOKIE['evento']);
         header("location:index.php?class=DashboardView&method=pospresenca");
     }
     header('location:index.php');
 }elseif($nowh >= $inicio && $nowh < $twoplus){
     if(givePresenca($id,$cpf)){
-        setcookie('evento',null, -1, '/');
+        setcookie('evento',null, time() - 3600);
         unset($_COOKIE['evento']);
         header("location:index.php?class=DashboardView&method=pospresenca");
     }
     header('location:index.php');
 }elseif(in_array(4,$grupos)){
     if(givePresenca($id,$cpf)){
-        setcookie('evento',null, -1, '/');
+        setcookie('evento',null, time() - 3600);
         unset($_COOKIE['evento']);
         header("location:index.php?class=DashboardView&method=pospresenca");
     }
     header('location:index.php');
 }elseif($gets >= 1){
     if(givePresenca($id,$cpf)){
-        setcookie('evento',null, -1, '/');
+        setcookie('evento',null, time() - 3600);
         unset($_COOKIE['evento']);
         header("location:index.php?class=DashboardView&method=pospresenca");
     }
     header('location:index.php');
 }else{
+    setcookie('evento',null, time() - 3600);
+    unset($_COOKIE['evento']);
+    session_destroy();
     echo "Você ainda não pode se inscrever nessa atividade";
 }
-
 
 ?>
